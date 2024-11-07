ffbe2337bbbd9d9dfa44dd7639931ed2d7fbd12c
kido-coder <anujin.lha@gmail.com>
2024-11-07 16:18:40 +0800

    test

--- udpserver.js
+++ udpserver.js
@@ -39,8 +39,11 @@ server.on('message', (msg, rinfo) => {
     //         ipadd.push(obj);
     //     }
     // }
+    const log = `Res: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
+    console.log(log);
+
     if (rinfo.port == waiting) {
-        const log = `Received: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
+        const log = `Res: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
         fs.appendFile('udp_data_log.txt', log, (err) => {
             if (err) {
                 console.error('Error writing to file:', err);
