4fb24d8ecc158ddbe110d25c21dcb1a059137978
kido-coder <anujin.lha@gmail.com>
2024-11-07 17:32:43 +0800

    test

--- udpserver.js
+++ udpserver.js
@@ -6,54 +6,76 @@ const HOST = '0.0.0.0';                                                         
 const server = dgram.createSocket('udp4');
 
 const [waiting, setWaiting] = useState([]);
-setWaiting(-1);
-
 var ipadd = [];
 
 useEffect(() => {
     setWaiting(-1);
-}, []);
 
-// Handle incoming messages
-server.on('message', (msg, rinfo) => {
-    if (msg.includes("SICTZ")) {
-        var node = msg.subarray(6, msg.length)
-        var obj = {
-            name: node,
-            address: rinfo.address,
-            port: rinfo.port
-        };
+    // Handle incoming messages
+    server.on('message', (msg, rinfo) => {
+        console.log(waiting);
+        if (msg.includes("SICTZ")) {
+            var node = msg.subarray(6, msg.length)
+            var obj = {
+                name: node,
+                address: rinfo.address,
+                port: rinfo.port
+            };
 
-        const exists = ipadd.some(item => item.address === obj.address && item.port === obj.port);
+            const exists = ipadd.some(item => item.address === obj.address && item.port === obj.port);
 
-        if (!exists) {
-            ipadd.push(obj);
+            if (!exists) {
+                ipadd.push(obj);
+            }
+        }
+
+        if (rinfo.port == waiting) {
+            const log = `Res: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
+            fs.appendFile('udp_data_log.txt', log, (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err);
+                }
+            });
+            console.log(log);
+            setWaiting(-1);
+        }
+
+        // Send a reply back to the sender
+        // const responseMessage = 'Bayarlalraa!';
+        // server.send(responseMessage, rinfo.port, rinfo.address, (error) => {
+        //     if (error) {
+        //         console.error(`Error sending response: ${error}`);
+        //     } else {
+        //         console.log(`Sent response to ${rinfo.address}:${rinfo.port}`);
+        //     }
+        // });
+        // console.log(ipadd)
+    });
+
+    // Cleanup if needed
+    // return () => {
+    //     server.off('message', yourHandlerFunction); // Replace with your actual handler
+    // };
+    function sendPacket() {
+        const request = 'data ug';
+        for (const obj of ipadd) {
+            while (waiting != -1) { };
+            server.send(request, obj.port, obj.address, (error) => {
+                if (error) {
+                    console.error(`Error sending response: ${error}`);
+                } else {
+                    console.log(`${obj.name} ruu Huselt ulgeesen`);
+                }
+            });
+            setWaiting(`${obj.port}`);
         }
     }
+    
+    setInterval(sendPacket, 1000);
+}, [waiting]);
 
-    console.log(waiting);
-    if (rinfo.port == waiting) {
-        const log = `Res: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
-        fs.appendFile('udp_data_log.txt', log, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            }
-        });
-        console.log(log);
-        setWaiting(-1);
-    }
+// Handle incoming messages
 
-    // Send a reply back to the sender
-    // const responseMessage = 'Bayarlalraa!';
-    // server.send(responseMessage, rinfo.port, rinfo.address, (error) => {
-    //     if (error) {
-    //         console.error(`Error sending response: ${error}`);
-    //     } else {
-    //         console.log(`Sent response to ${rinfo.address}:${rinfo.port}`);
-    //     }
-    // });
-    // console.log(ipadd)
-});
 
 server.on('error', (err) => {
     console.error(`Server error: ${err.stack}`);
@@ -66,19 +88,3 @@ server.bind(PORT, HOST, () => {
 });
 
 
-function sendPacket() {
-    const request = 'data ug';
-    for (const obj of ipadd) {
-        while (waiting != -1) {};
-        server.send(request, obj.port, obj.address, (error) => {
-            if (error) {
-                console.error(`Error sending response: ${error}`);
-            } else {
-                console.log(`${obj.name} ruu Huselt ulgeesen`);
-            }
-        });
-        setWaiting(`${obj.port}`);
-    }
-}
-
-setInterval(sendPacket, 1000);
