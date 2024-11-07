a18f6fe401feb96c1413d211f891af69db4af5c9
kido-coder <anujin.lha@gmail.com>
2024-11-07 16:15:53 +0800

    test

--- udpserver.js
+++ udpserver.js
@@ -4,31 +4,51 @@ const fs = require('fs');
 const PORT = 8888;                                                                      // Port to listen for incoming UDP packets
 const HOST = '0.0.0.0';                                                                 // Listen on all available IPs
 const server = dgram.createSocket('udp4');
+
+const [waiting, setWaiting] = useState([]);
+setWaiting(-1);
+
 var ipadd = [];
+var obj = {
+    key1: "#0001",
+    key2: '202.70.34.27',
+    key3: '8888'
+};
+ipadd.push(obj);
+obj = {
+    key1: "#0002",
+    key2: '202.70.34.27',
+    key3: '51104'
+};
+
+ipadd.push(obj);
 
 // Handle incoming messages
 server.on('message', (msg, rinfo) => {
-    if (msg.includes("SICTZ")) {
-        var node = msg.subarray(5, msg.length)
-        var obj = {
-            key1: node,
-            key2: rinfo.address,
-            key3: rinfo.port
-        };
+    // if (msg.includes("SICTZ")) {
+    //     var node = msg.subarray(5, msg.length)
+    //     var obj = {
+    //         key1: node,
+    //         key2: rinfo.address,
+    //         key3: rinfo.port
+    //     };
 
-        const exists = ipadd.some(item => item.key2 === obj.key2 && item.key3 === obj.key3);
+    //     const exists = ipadd.some(item => item.key2 === obj.key2 && item.key3 === obj.key3);
 
-        if (!exists) {
-            ipadd.push(obj);
-        }
+    //     if (!exists) {
+    //         ipadd.push(obj);
+    //     }
+    // }
+    if (rinfo.port == waiting) {
+        const log = `Received: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
+        fs.appendFile('udp_data_log.txt', log, (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+            }
+        });
+        console.log(log);
+        setWaiting(-1);
     }
-    const log = `Received: ${msg} from ${rinfo.address}:${rinfo.port}\n`;
-    fs.appendFile('udp_data_log.txt', log, (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-        }
-    });
-
     // Send a reply back to the sender
     // const responseMessage = 'Bayarlalraa!';
     // server.send(responseMessage, rinfo.port, rinfo.address, (error) => {
@@ -38,7 +58,7 @@ server.on('message', (msg, rinfo) => {
     //         console.log(`Sent response to ${rinfo.address}:${rinfo.port}`);
     //     }
     // });
-    console.log(ipadd)
+    // console.log(ipadd)
 });
 
 server.on('error', (err) => {
@@ -55,6 +75,7 @@ server.bind(PORT, HOST, () => {
 function sendPacket() {
     const request = 'data ug';
     for (const obj of ipadd) {
+        while (waiting != -1) {};
         server.send(request, obj.key3, obj.key2, (error) => {
             if (error) {
                 console.error(`Error sending response: ${error}`);
@@ -62,6 +83,7 @@ function sendPacket() {
                 console.log(`${obj.key1} ruu Huselt ulgeesen`);
             }
         });
+        setWaiting(`${obj.key3}`);
     }
 }
 
