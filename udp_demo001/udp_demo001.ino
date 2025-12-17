54ecfdcbac151baef1fb90552399921abb1b8dd9
kido-coder <anujin.lha@gmail.com>
2025-12-17 13:42:13 +0800

    Ethernet logic nuhtsluud nemsen

--- udp_demo001/udp_demo001.ino
+++ udp_demo001/udp_demo001.ino
@@ -1,68 +1,96 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#include <string.h>
 
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+char packetBuffer[5];
+char ReplyBuffer[] = "A:001";
+char FirstBuffer[] = "R:001!";
+char end[] = "!";
+
+#define link     0
+#define dhcp     1
+#define server   2
+char Status[] = { 0, 0, 0 };
 
 // Бүх төхөөрөмж онцгой хаягтай байх ёстой
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xF0, 0x01};
-unsigned int localPort = 9999;
-IPAddress remote(183,177,105,156), node_ip;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xF0, 0x01 };
+IPAddress remote(183, 177, 105, 156);
+unsigned int localPort = 9999, remotePort = 8888;
 EthernetUDP Udp;
 
-// Registration buffer
-char FirstBuffer[] = "R:001!";
 int count = 0;
 
 void setup() {
   Serial.begin(9600);
-  if (Ethernet.begin(mac) != 0) {
-    Serial.print("My IP address: ");
+  if (Ethernet.begin(mac, 1000, 1000) != 0) {
+    Serial.print("My IP address in setup: ");
     Serial.println(Ethernet.localIP());
-  }
-  
-  // UPD порт нээж, өгөгдөл хүлээн авах (listen) боломжтой болгоно
-  Udp.begin(localPort);
+    Status[dhcp] = 1;
 
-  //UDP-р R-г илгээж байна
-  Udp.beginPacket(remote, 8888);
-  Udp.write(FirstBuffer);
-  Udp.endPacket();
+    // UPD порт нээж, өгөгдөл хүлээн авах (listen) боломжтой болгоно
+    Udp.begin(localPort);
+
+    //UDP-р R-г илгээж байна
+    Udp.beginPacket(remote, remotePort);
+    Udp.write(FirstBuffer);
+    Udp.endPacket();
+  } else {
+    Serial.println("DHCP failed in setup...");
+  }
 }
 
 void loop() {
-  // Irsen Packet-iin urt (byte)
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Udp.read(packetBuffer, 10);
-    String inc = packetBuffer;
-    Serial.println(inc);
-    if (inc[0] == 'C') {
-      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      char ReplyBuffer[] = "A:001";
-      char end[] = "!";
-      char charArray[6];
-      itoa(count, charArray, 10); // Convert int to char array in base 10
+  Status[link] = Ethernet.linkStatus();
+  Serial.print(Status[link] + 48);
+  Serial.print(Status[dhcp] + 48);
+  Serial.print(Status[server] + 48);
+  if (Status[link] == LinkON) {
+    if (Status[dhcp]) {
+      int packetSize = Udp.parsePacket();
+      if (packetSize) {
+        Udp.read(packetBuffer, packetSize);
+        Serial.println(packetBuffer);
+      }
+      if (Status[server]) {
+        // Cyclic request
+        if (packetBuffer[0] == 'C') {
+          Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+          char charArray[6];
+          itoa(count, charArray, 10);  // Convert int to char array in base 10
 
-      Udp.write(ReplyBuffer);
-      Udp.write(charArray);
-      Udp.write(end);
-      Udp.endPacket();
-    }
-    if (inc[0] == 'O') {
-      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-      char ReplyBuffer[] = "A:001999";
-      char end[] = "!";
-      char charArray[6];
-      itoa(count, charArray, 10); // Convert int to char array in base 10
+          Udp.write(ReplyBuffer);
+          Udp.write(charArray);
+          Udp.write(end);
+          Udp.endPacket();
+        }
 
-      Udp.write(ReplyBuffer);
-      Udp.write(charArray);
-      Udp.write(end);
-      Udp.endPacket();
+        // Control command
+        if (packetBuffer[0] == 'O') {
+        }
+      } else {
+        if (packetBuffer[0] == 'X') {
+          Status[server] = 1;
+        } else {
+          Udp.beginPacket(remote, remotePort);
+          Udp.write(FirstBuffer);
+          Udp.endPacket();
+          Serial.println("Sent X");
+        }
+      }
+    } else {
+      if (Ethernet.begin(mac, 1000, 1000) != 0) {
+        Serial.print("My IP address in loop: ");
+        Serial.println(Ethernet.localIP());
+        Status[dhcp] = 1;
+        Udp.begin(localPort);
+      } else {
+        Serial.println("DHCP failed in loop...");
+      }
     }
-    count ++;
-    for(int i = 0; i < packetSize;i++) packetBuffer[i] = 0;
+  } else {
+    Status[dhcp] = 0;
+    Status[server] = 0;
   }
+  count ++;
+  delay (1000);
 }
